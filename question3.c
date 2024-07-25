9bfc4b8725e21412f4b3c06392c0240b8a18fd65
navyathakur04 <142678860+navyathakur04@users.noreply.github.com>
2024-07-25 10:22:13 +0530

    Create question3.c

---
+++ question3.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char* str) {
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++) {
+        char temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
+    }
+}
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, 100, stdin);
+    str[strcspn(str, "\n")] = 0;
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
